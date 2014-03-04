fa1dc663c5a8ee1a95f3e81235809591878f40f1
VlasovSergey <qlir@ya.ru>
2014-03-04 16:28:45 +0400

    adds oneDriveDb.js for database management

---
+++ platforms/wp8/www/js/oneDriveDb.js
@@ -0,0 +1,122 @@
+/**
+ * Created by sergey.vlasov on 3/3/14.
+ */
+function DbManager (name){
+    var DB_NAME = name,
+        db,
+
+        create = function(){
+            indexedDB.deleteDatabase(DB_NAME);
+            console.log('db delete');
+
+            var idbRequest = indexedDB.open(DB_NAME, 2);
+            idbRequest.onupgradeneeded =
+                function(event) {
+                    db = event.target.result;
+                    // Create an objectStore to hold information about our customers. We're
+                    // going to use "ssn" as our key path because it's guaranteed to be
+                    // unique.
+                    var objectStore = db.createObjectStore("loadState", { keyPath: "idFile" });
+                    // Create an index to search customers by name. We may have duplicates
+                    // so we can't use a unique index.
+                    objectStore.createIndex("state", "state", { unique: false });
+                    // Store values in the newly created objectStore.
+                    console.log('create ok');
+                };
+        },
+
+        add = function(data) {
+            if(db) {
+                var transaction = db.transaction(["loadState"], "readwrite");
+                // Do something when all the data is added to the database.
+                transaction.oncomplete = function(event) {
+                    console.log('transaction ok');
+                    //alert("All done!");
+                };
+                transaction.onerror = function(event) {
+                    console.log('transaction err');
+                    // Don't forget to handle errors!
+                };
+
+                var objectStore = transaction.objectStore("loadState");
+                var request = objectStore.add(data);
+                request.onsuccess = function(event) {
+                    console.log('add ok');
+                    // event.target.result == customerData[i].ssn;
+                };
+            }else {
+                var idbRequest = indexedDB.open(DB_NAME, 2);
+                idbRequest.onerror = onError;
+                idbRequest.onsuccess = function(event){
+                    db = event.target.result;
+                    add(data);
+                }
+            }
+        },
+
+        readOne = function(id, onsuccess){
+            if(db){
+                db.transaction("loadState").objectStore("loadState").get(id).onsuccess = function(event) {
+                    //console.log('ret ',event.target.result);
+                    onsuccess(event.target.result);
+                };
+            }else {
+                var idbRequest = indexedDB.open(DB_NAME, 2);
+                idbRequest.onerror = onError;
+                idbRequest.onsuccess = function(event){
+                    db = event.target.result;
+                    readOne(id,onsuccess);
+                    //console.log(readOne(id));
+                }
+            }
+        },
+
+        remove = function(id) {
+            if (db) {
+                var request = db.transaction(["loadState"], "readwrite")
+                    .objectStore("loadState")
+                    .delete(id);
+                request.onsuccess = function(event) {
+                    console.log('remove ok');
+                };
+            } else {
+                var idbRequest = indexedDB.open(DB_NAME, 2);
+                idbRequest.onerror = onError;
+                idbRequest.onsuccess = function(event){
+                    db = event.target.result;
+                    remove(id);
+                    //console.log(readOne(id));
+                }
+            }
+        },
+
+        replace = function(id, newObj) {
+            if (db) {
+                var request = db.transaction(["loadState"], "readwrite")
+                    .objectStore("loadState")
+                    .delete(id);
+                request.onsuccess = function(event) {
+                    add(newObj);
+                };
+            } else {
+                var idbRequest = indexedDB.open(DB_NAME, 2);
+                idbRequest.onerror = onError;
+                idbRequest.onsuccess = function(event) {
+                    db = event.target.result;
+                    replace(id, newObj);
+                }
+            }
+        },
+
+        onError = function(err) {
+            console.log('Error: ' + err);
+        };
+
+    return {
+        create : create,
+        add : add,
+        read: readOne,
+        remove: remove,
+        replace: replace
+    }
+}
