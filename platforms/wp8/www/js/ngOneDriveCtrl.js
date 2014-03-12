8e0543a071d4f6a67ff366695f6e2eeb1b1804b2
VlasovSergey <qlir@ya.ru>
2014-03-12 16:41:35 +0400

    code refact

--- platforms/wp8/www/js/ngOneDriveCtrl.js
+++ platforms/wp8/www/js/ngOneDriveCtrl.js
@@ -5,44 +5,43 @@
         ROOT_TITLE = 'OneDrive',
         CLIENT_ID = "0000000048113444",
         REDIRECT_URI = "https://login.live.com/oauth20_desktop.srf",
-        DOWNLOADED_STATE = 'downloaded',
-        NOT_DOWNLOADED_STATE = 'notDownloaded',
-        PROGRESS_STATE = 'progress';
+        DOWNLOADED_STATE = 1,
+        NOT_DOWNLOADED_STATE = 0,
+        PROGRESS_STATE = 2;
     // Define the controller on the module.
     // Inject the dependencies.
     // Point to the controller definition function.
     angular.module('app', []).controller(controllerId, ['$scope', '$http', '$q' , function ($scope, $http, $q) {
-        document.addEventListener("backbutton", onBackKeyDown, true);
-        function onBackKeyDown() {
-            toPreFolder();
-        }
         var oneDriveManager = new OneDriveManager(),
             dataBase,
-            directoryId = [],
+            directoryIds = [],
 
             getFilesByParameter =  function(parameter ,value){
                 return $scope.filesAndFolders.filter(
                     function (obj) {
                         return obj[parameter] === value;
-                    })[0];
+                    });
             },
 
-            addDownloadState = function(data){
-                data.forEach(function(obj){
-                    if(obj.type !== 'folder'){
-                        obj.state = NOT_DOWNLOADED_STATE;
+            addDownloadState = function(fileList){
+                fileList.forEach(function(fileInfo){
+                    if(fileInfo.type !== 'folder'){
+                        setFileState(fileInfo, NOT_DOWNLOADED_STATE);
                     }
                 });
             },
 
+            setFileState = function(file, state) {
+                file.state = state;
+            },
+
             toPreFolder = function () {
-                if(ROOT_TITLE == $scope.directory)
-                {
+                if ($scope.directory == ROOT_TITLE) {
                     navigator.app.exitApp();
-                    return;
+                    //return;
                 }
                 var dirArr = $scope.directory.split('/'),
-                    directoryToLoad = directoryId.length - 2 >= 0 ? directoryId[directoryId.length - 2] + '/files' : null;
+                    directoryToLoad = directoryIds.length - 2 >= 0 ? directoryIds[directoryIds.length - 2] + '/files' : null;
 
                 oneDriveManager.loadFilesData(directoryToLoad).then(
                     function (data) {
@@ -50,7 +49,7 @@
 
                         $scope.filesAndFolders = data;
 
-                        directoryId.splice(directoryId.length - 1, 1);
+                        directoryIds.splice(directoryIds.length - 1, 1);
 
                         dirArr.splice(dirArr.length - 1, 1);
                         $scope.directory = dirArr.join("/");
@@ -61,24 +60,75 @@
             },
 
             updateStateOfDb = function(){
-                $scope.filesAndFolders.forEach(function(obj){
-                    if (obj.type != 'folder'){
-                        dataBase.readItem(obj.id, function(fileData){
+                $scope.filesAndFolders.forEach(function(fileInfo){
+                    if (fileInfo.type != 'folder'){
+                        dataBase.readItem(fileInfo.id, function(fileData){
                             if (!fileData) return;
                             console.log(JSON.stringify(fileData));
-                            var file = getFilesByParameter('id', fileData.id);
-                            file.state = fileData.state;
+                            var file = getFilesByParameter('id', fileData.id)[0];
+                            setFileState(file, fileData.state);
                             file.localPath = fileData.localPath;
                             console.log(file.name+'  ' +file.startProgress+'    '+file.state);
                             if (!file.startProgress && file.state == PROGRESS_STATE){
-                                $scope.downloadFile(file);
+                                downloadFile(file);
                             }
                             $scope.$apply();
                         });
                     }
                 });
+            },
+            
+            downloadFile=function(file){
+                var onSuccess = function(filePath){
+                        var fileNew = getFilesByParameter('name', file.name)[0];
+                        console.log('onSuccess ' + fileNew.name);
+                        fileNew.startProgress = false;
+                        fileNew.localPath = filePath;
+                        fileNew.state = DOWNLOADED_STATE;
+                        dataBase.addItem({
+                            id: fileNew.id,
+                            state: DOWNLOADED_STATE,
+                            url: fileNew.source,
+                            localPath: fileNew.localPath
+                        });
+                        $scope.$apply();
+                    },
+                    onError = function(res){
+                        console.log('onError>>>>>>>>>> '+res);
+                        var fileNew = getFilesByParameter('name', file.name)[0];
+
+                        fileNew.startProgress = false;
+                        fileNew.state = NOT_DOWNLOADED_STATE;
+                        dataBase.removeItem(fileNew.id);
+                        $scope.$apply();
+                    },
+                    onProgress = function(res){
+                        console.log('onProgress ' + file.name);
+
+                        var fileNew = getFilesByParameter('id', file.id)[0];
+
+                        fileNew.state = PROGRESS_STATE;
+                        fileNew.progress = res;
+
+                        $scope.$apply();
+                    };
+
+                oneDriveManager.downloadFile(file.source, $scope.directory + '/' + file.name, onSuccess, onError, onProgress);
+            },
+            
+            saveStageToDataBase = function(file){
+                file.state = PROGRESS_STATE;
+                file.startProgress = true;
+                file.progress = "0";
+                dataBase.addItem({
+                    id: file.id,
+                    state: PROGRESS_STATE,
+                    url: file.source,
+                    localPath:file.localPath
+                });
             };
 
+        document.addEventListener("backbutton", toPreFolder, true);
         oneDriveManager.setClientId(CLIENT_ID);
         oneDriveManager.setRedirectUri(REDIRECT_URI);
         oneDriveManager.onControllerCreated($http, $q);
@@ -86,7 +136,7 @@
         $scope.directory = ROOT_TITLE;
 
         $scope.displayFolder = function (folderName) {
-            var folderId = getFilesByParameter('name', folderName).id;
+            var folderId = getFilesByParameter('name', folderName)[0].id;
 
             oneDriveManager.loadFilesData(folderId + '/files')
                 .then(
@@ -94,8 +144,7 @@
                     addDownloadState(data);
                     $scope.filesAndFolders = data;
                     $scope.directory += '/' + folderName;
-                    directoryId.push(folderId);
-
+                    directoryIds.push(folderId);
                     updateStateOfDb();
                 }
             );
@@ -106,61 +155,16 @@
             cordova.exec(null, null, 'FileOpening', 'open', [file.localPath]);
         };
 
-        $scope.downloadFile = function(file){
-            file.state = PROGRESS_STATE;
-            file.startProgress = true;
-            file.progress = "0";
-            dataBase.addItem({
-                id: file.id,
-                state: PROGRESS_STATE,
-                url: file.source,
-                localPath:file.localPath
-            });
-
-            var onSuccess = function(filePath){
-                    var fileNew = getFilesByParameter('name', file.name);
-                    console.log('onSuccess ' + fileNew.name);
-                    fileNew.startProgress = false;
-                    fileNew.localPath = filePath;
-                    fileNew.state = DOWNLOADED_STATE;
-                    dataBase.addItem({
-                        id: fileNew.id,
-                        state: DOWNLOADED_STATE,
-                        url: fileNew.source,
-                        localPath: fileNew.localPath
-                    });
-                    $scope.$apply();
-                },
-                onError = function(res){
-                    console.log('onError>>>>>>>>>> '+res);
-                    var fileNew = getFilesByParameter('name', file.name);
-
-                    fileNew.startProgress = false;
-                    fileNew.state = NOT_DOWNLOADED_STATE;
-                    dataBase.removeItem(fileNew.id);
-                    $scope.$apply();
-                },
-                onProgress = function(res){
-                    console.log('onProgress ' + file.name);
-
-                    var fileNew = getFilesByParameter('id', file.id);
-
-                    fileNew.state = PROGRESS_STATE;
-                    fileNew.progress = res;
-
-                    $scope.$apply();
-                };
-
-            oneDriveManager.downloadFile(file.source, $scope.directory + '/' + file.name, onSuccess, onError, onProgress);
+        $scope.onClickDownloadButton = function(file){
+            saveStageToDataBase(file);
+            downloadFile(file);
         };
 
         oneDriveManager.loadUserInfo().then(
             function (userInfo) {
-
                 DbManager.createDB(userInfo.id, "loadState",'id',['state', 'url', 'localPath'], function(db){
                     dataBase = db;
                 });
-
                 $scope.userName = userInfo.name;
             }
         );
--- platforms/wp8/www/js/oneDriveDb.js
+++ platforms/wp8/www/js/oneDriveDb.js
@@ -1,79 +1,82 @@
 /**
  * Created by sergey.vlasov on 3/3/14.*/
-DbManager = {
-    createDB: function(nameDB, tableName, keyPath, parametersArray, onSuccess){
-        console.log('start ok'+ nameDB);
-        var idbRequest = indexedDB.open(nameDB, 2);
-        console.log('create start');
-        idbRequest.onupgradeneeded =
-            function(event) {
-                console.log('create start');
-                var db = event.target.result,
-                    objectStore = db.createObjectStore(tableName, { keyPath: keyPath });
-                console.log('create 3333');
-                parametersArray.forEach(function(nameIndex){
-                    console.log(nameIndex);
-                    objectStore.createIndex(nameIndex, nameIndex, { unique: false });
-                });
-                console.log('create ok');
+(function(){
+    var OneDriveDB = function(DB,tablename) {
+        var db = DB,
+            tableName = tablename,
+            getObjectStore = function(){
+                console.log(tableName);
+                return  db.transaction([tableName], "readwrite").objectStore(tableName);
+
+            },
+            addItem = function(data, onsuccess, onerror) {
+                var transaction = db.transaction([tableName], "readwrite"),
+                    objectStore = transaction.objectStore(tableName),
+                    request = objectStore.add(data);
+
+                transaction.onerror = function() {
+                    console.log('db    replase');
+                    replaceItem(data.id, data);
+                };
+
+                request.onsuccess = onsuccess;
+                request.onerror = onerror;
+            },
+
+            readItem = function(id, onsuccess){
+                getObjectStore().get(id).onsuccess = function(event) {
+                    onsuccess(event.target.result);
+                };
+            },
+
+            removeItem = function(id, onsuccess, onerror) {
+                var request = getObjectStore().delete(id);
+                request.onsuccess = onsuccess;
+                request.onerror = onerror;
+            },
+
+            replaceItem = function(id, newObj, onsuccess, onerror) {
+                var request = getObjectStore().delete(id);
+                request.onsuccess = function() {
+                    addItem(newObj, onsuccess, onerror);
+                };
+                request.onerror = onerror;
             };
-        idbRequest.onsuccess = function(event){
-            console.log('ok');
-            onSuccess(new OneDriveDB(event.target.result, tableName));
-        }
-    },
-    deleteDB: function(nameDB){
+
+        return {
+            addItem : addItem,
+            readItem: readItem,
+            removeItem: removeItem,
+            replaceItem: replaceItem
+        };
+    };
+
+    window.DbManager = window.DbManager || {
+        createDB: function(nameDB, tableName, keyPath, parametersArray, onSuccess){
+            console.log('start ok'+ nameDB);
+            var idbRequest = indexedDB.open(nameDB, 2);
+            console.log('create start');
+            idbRequest.onupgradeneeded =
+                function(event) {
+                    console.log('create start');
+                    var db = event.target.result,
+                        objectStore = db.createObjectStore(tableName, { keyPath: keyPath });
+                    console.log('create 3333');
+                    parametersArray.forEach(function(nameIndex){
+                        console.log(nameIndex);
+                        objectStore.createIndex(nameIndex, nameIndex, { unique: false });
+                    });
+                    console.log('create ok');
+                };
+            idbRequest.onsuccess = function(event){
+                console.log('ok');
+                onSuccess(new OneDriveDB(event.target.result, tableName));
+            }
+        },
+
+        deleteDB : function(nameDB) {
             indexedDB.deleteDatabase(nameDB);
             console.log('del ok');
-    }
-}
-
-function OneDriveDB(DB,tablename){
-    var db = DB,
-        tableName = tablename,
-        getObjectStore = function(){
-            console.log(tableName);
-            return  db.transaction([tableName], "readwrite").objectStore(tableName);
-
-        },
-        addItem = function(data, onsuccess, onerror) {
-            var transaction = db.transaction([tableName], "readwrite"),
-                objectStore = transaction.objectStore(tableName),
-                request = objectStore.add(data);
-
-            transaction.onerror = function(event) {
-                console.log('db    replase');
-                replaceItem(data.id, data);
-            };
-
-            request.onsuccess = onsuccess;
-            request.onerror = onerror;
-        },
-
-        readItem = function(id, onsuccess){
-            getObjectStore().get(id).onsuccess = function(event) {
-                onsuccess(event.target.result);
-            };
-        },
-
-        removeItem = function(id, onsuccess, onerror) {
-            var request = getObjectStore().delete(id);
-            request.onsuccess = onsuccess;
-            request.onerror = onerror;
-        },
-
-        replaceItem = function(id, newObj, onsuccess, onerror) {
-            var request = getObjectStore().delete(id);
-            request.onsuccess = function(event) {
-                addItem(newObj, onsuccess, onerror);
-            };
-            request.onerror = onerror;
+            }
         };
-
-    return {
-        addItem : addItem,
-        readItem: readItem,
-        removeItem: removeItem,
-        replaceItem: replaceItem
-    };
-}
+}());
