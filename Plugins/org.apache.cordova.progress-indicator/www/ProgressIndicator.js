fb05eceabba6d9baa02936f360612618859b81a3
VlasovSergey <qlir@ya.ru>
2014-04-14 17:10:04 +0400

    fixes ProgressIndicator.js

--- Plugins/org.apache.cordova.progress-indicator/www/ProgressIndicator.js
+++ Plugins/org.apache.cordova.progress-indicator/www/ProgressIndicator.js
@@ -1,11 +1,10 @@
-cordova.define("org.apache.cordova.progress-indicator.ProgressIndicator", function(require, exports, module) { // forked from https://github.com/markeeftb/FileOpener
-    module.exports = {
-        show: function (tapDisable) {
-            cordova.exec(null, null, "ProgressIndicator", "show", [!!tapDisable]);
-        },
+module.exports = {
+    show: function (tapDisable) {
+        cordova.exec(null, null, "ProgressIndicator", "show", [!!tapDisable]);
+    },
 
-        hide: function () {
-            cordova.exec(null, null, "ProgressIndicator", "hide", []);
-        }
+    hide: function () {
+        cordova.exec(null, null, "ProgressIndicator", "hide", []);
     }
-});
+}
+
