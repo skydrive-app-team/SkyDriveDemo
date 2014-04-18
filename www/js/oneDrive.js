a2078d3498ece42c7ceb1d88a9eece77add70dac
VlasovSergey <qlir@ya.ru>
2014-04-18 16:47:36 +0400

    fixes load signInUrl for ios

--- www/js/oneDrive.js
+++ www/js/oneDrive.js
@@ -51,7 +51,8 @@ function OneDriveManager(_clientId, _redirectUri) {
             userInfoUrl = "https://apis.live.net/v5.0/me/?method=GET&interface_method=undefined&pretty=false&return_ssl_resources=false&x_http_live_library=Web%2Fchrome_5.5&suppress_redirects=true&"+accessToken;
             filesUrlForDirectory = "https://apis.live.net/v5.0/%folderID%?method=GET&interface_method=undefined&pretty=false&return_ssl_resources=false&x_http_live_library=Web%2Fchrome_5.5&suppress_redirects=true&"+accessToken;
             singOutUrl = "http://login.live.com/oauth20_logout.srf?" + accessToken + "&client_id=" + clientId + "&display=touch&locale=en&response_type=token&scope=wl.skydrive&state=redirect_type=auth&display=touch&request_ts=1392886026466&redirect_uri=x-wmapp0%253Awww%252Findex.html&response_method=url&secure_cookie=false&redirect_uri=" + redirectUri;
-            signInUrl = "https://login.live.com/oauth20_authorize.srf?client_id=" + clientId + "&display=touch&locale=en&response_type=token&scope=wl.skydrive&state=redirect_type=auth&display=touch&request_ts={0}&redirect_uri=x-wmapp0%253Awww%252Findex.html&response_method=url&secure_cookie=false&redirect_uri=" + redirectUri;
+            signInUrl = "https://login.live.com/oauth20_authorize.srf?client_id=" + clientId + "&display=touch&locale=en&response_type=token&scope=wl.skydrive&state=redirect_type=auth&display=touch&redirect_uri=x-wmapp0%253Awww%252Findex.html&response_method=url&secure_cookie=false&redirect_uri=" + redirectUri;
+            //'https://login.live.com/oauth20_authorize.srf?client_id=0000000048113444&display=touch&locale=en&response_type=token&scope=wl.skydrive&state=redirect_type=auth&display=touch&request_ts={0}&redirect_uri=x-wmapp0%253Awww%252Findex.html&response_method=url&secure_cookie=false&redirect_uri=http://skydrivesuperdemo.com/skyDrive/index.html'
         };
     clientId = _clientId;
     redirectUri = _redirectUri;
@@ -92,13 +93,22 @@ function OneDriveManager(_clientId, _redirectUri) {
 
         signIn: function(onSuccess) {
             ProgressIndicator.hide();
-            var inAppBrowser = window.open(signInUrl, '_blank', 'location=no'),
+            //window.open(signInUrl, '_blank', 'location=no');
+            //alert(signInUrl);
+            var inAppBrowser = window.open(signInUrl,'_blank', 'location=no');
                 deferred = q.defer();
             ProgressIndicator.show(true);
             inAppBrowser.addEventListener('loadstop',function(e){
+                                          //alert('loadstop');
                 ProgressIndicator.hide();
             });
+            
+            /*inAppBrowser.addEventListener('loaderror', function(e){
+                                          alert('loaderror::'+e.message);
+                                          });*/
+            
             inAppBrowser.addEventListener('loadstart', function (e) {
+                //alert('staart url='+e.url);
                 if (e.url.indexOf("access_token=") > 0) {
                     accessToken = getAccessTokenFromURL(e.url);
                     deferred.resolve(accessToken);
@@ -107,6 +117,7 @@ function OneDriveManager(_clientId, _redirectUri) {
             });
 
             inAppBrowser.addEventListener('exit', function(e){
+                
                 console.log("exit:");
                 if(!accessToken){
                     navigator.app.exitApp();
@@ -119,10 +130,9 @@ function OneDriveManager(_clientId, _redirectUri) {
         signOut: function() {
             var inAppBrowser = window.open(singOutUrl, '_blank', 'location=no');
             ProgressIndicator.show(true);
-            inAppBrowser.addEventListener('loadstart', function(e){
+            inAppBrowser.addEventListener('loadstart', function(e) {
                 if (e.url.indexOf(redirectUri) === 0) {
                     inAppBrowser.close();
-                    //ProgressIndicator.hide();
                     location.reload();
                 }
             });
