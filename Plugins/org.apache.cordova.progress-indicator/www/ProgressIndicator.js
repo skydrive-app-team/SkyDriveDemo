0116baf38c4e755015906893aca44a071c03c47e
VlasovSergey <qlir@ya.ru>
2014-04-21 18:33:30 +0400

    adds property color in progress-indicator

--- Plugins/org.apache.cordova.progress-indicator/www/ProgressIndicator.js
+++ Plugins/org.apache.cordova.progress-indicator/www/ProgressIndicator.js
@@ -1,10 +1,13 @@
-//
+//progressIndicator plugin
 module.exports = {
-    show: function (tapDisable,message) {
-        cordova.exec(null, null, "ProgressIndicator", "show", [!!tapDisable, message?message:"Waiting"]);
+    show: function (tapDisable, message, color) {
+        cordova.exec(null, null, "ProgressIndicator", "show", [!!tapDisable, message?message:"Waiting", color]);
     },
 
     hide: function () {
         cordova.exec(null, null, "ProgressIndicator", "hide", []);
     }
 }
+
+
+
